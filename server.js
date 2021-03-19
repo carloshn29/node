033b443dee7c75a1dc3178e861e03399104d8704
ProfCarlos <hernandeznc@ipn.mx>
2021-03-19 13:28:32 -0600

    cambio de nombre a server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+  res.send("Hola Martin y Jessica");
+});
+
+const port = process.env.PORT || 8081;
+
+app.listen(8081, () => {
+    console.log("hola desde la consola");
+});
