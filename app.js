d3643544a30dd8eb1ec5c470394230eb4d8356d3
ProfCarlos <hernandeznc@ipn.mx>
2021-03-19 13:01:26 -0600

    se agrego la variable del puerto

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/',(req,res) => {
   res.send("Hola Martin y Jessica");
 });
 
+const port = process.env.port || 3000;
+
 app.listen(3000, () => {
     console.log("hola desde la consola");
 });
